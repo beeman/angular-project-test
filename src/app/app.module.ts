fd6efd06228610ba5bb36e9bf7453d786eee1755
Bram Borggreve <borggreve@gmail.com>
2018-04-07 01:39:27 -0500

    Implement app that uses packages built in beeman/angular-project-demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { UiLayoutComponent, UiModule } from '@beeman/angular-ui'
 
 
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+const routes = [
+  {
+    path: '', component: UiLayoutComponent, children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'about', component: AboutComponent },
+      { path: 'home', component: HomeComponent },
+    ]
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UiModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
